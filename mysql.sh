116b30bbb441fdb8369c5b522b0a7bf6b60e6d2c
devops-learning-aws <aswiniannameti190499>
2025-08-03 18:34:45 +0530

    updated

--- mysql.sh
+++ mysql.sh
@@ -2,48 +2,52 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRPT_NAME-$TIMESTAMP.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-echo "please enter you DB Password"
+echo "Please enter DB password:"
 read -s mysql_root_password
 
 VALIDATE(){
-    if [ $1 -ne 0 ] 
-    then 
-       echo -e "$2 $R falled $N"
-       exit 1
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
     else
-       echo -e "$2 $G success $N"
-    fi      
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
 
-if [ $USERID -ne 0 ] 
+if [ $USERID -ne 0 ]
 then
-     echo "please run wiith super user access"
-     exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-     echo "you are a super user"
+    echo "You are super user."
 fi
 
-dnf install mysql -y  &>>$LOGFILE
-VALIDATE $? "Installing mysql server"
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enabling MySQL Server"
 
-systemctl start mysqld $>>$LOGFILE
-VALIDATE $? "starting mysql"
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
 
-mysql -h 172.31.90.18 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-if [ $? -ne 0 ] 
-then 
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "setting up root password"
+    VALIDATE $? "MySQL Root password Setup"
 else
-    echo "alreeady setupped root password skippng up"
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
